af508d9a6df6a4ec5b11b0f74294b1e763684474
Adam Aslan <adamaslan@Adams-MacBook-Air-2.local>
2023-07-14 16:49:22 -0700

    ethos

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,5 @@
-<h1>Welcome to the Drinks Food Life Blog</h1>
+<<h1>Welcome to the Drinks Food Life Blog</h1>
 <p>Visit <a href="https://www.drinksfoodlife.com">Drinks Food Life!</a> to see our main site :)</p>
+<p>this blog gives a bit of a behind the scenes view of Drinks Food Life Magazine and our branded content</p>
+
+<p> We love brands and their content</p>
